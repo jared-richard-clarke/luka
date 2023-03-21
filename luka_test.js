223121ae4e5faeb3b19b8119691a807961c9035c
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-03-21 13:18:54 -0700

    Use strict assertion with error messages

--- luka_test.js
+++ luka_test.js
@@ -1,71 +1,91 @@
-import { assertEquals } from "https://deno.land/std@0.180.0/testing/asserts.ts";
+import { assertStrictEquals } from "https://deno.land/std@0.180.0/testing/asserts.ts";
 import op from "./luka.js";
 
-Deno.test("test negate", () => {
+Deno.test("negation", () => {
     // distributive: -(x + y) = -x + -y
-    assertEquals(op.neg(7 + 11), op.neg(7) + op.neg(11));
+    assertStrictEquals(op.neg(7 + 11), op.neg(7) + op.neg(11), "distributive");
     // double negation: --x = x
-    assertEquals(op.neg(op.neg(7)), 7);
+    assertStrictEquals(op.neg(op.neg(7)), 7, "double negation");
 });
 
-Deno.test("test add", () => {
+Deno.test("addition", () => {
     // commutative: x + y = y + x
-    assertEquals(op.add(7, 11), op.add(11, 7));
+    assertStrictEquals(op.add(7, 11), op.add(11, 7), "commutative");
     // associative: (x + y) + z = x + (y + z)
-    assertEquals(op.add(1, op.add(2, 3)), op.add(op.add(1, 2), 3));
+    assertStrictEquals(
+        op.add(1, op.add(2, 3)),
+        op.add(op.add(1, 2), 3),
+        "associative",
+    );
     // distributive: x * (y + z) = (x * y) + (x * z)
-    assertEquals(2 * op.add(3, 4), op.add(2 * 3, 2 * 4));
+    assertStrictEquals(2 * op.add(3, 4), op.add(2 * 3, 2 * 4), "distributive");
     // identity: x + 0 = x
-    assertEquals(op.add(7, 0), 7);
+    assertStrictEquals(op.add(7, 0), 7, "identity");
 });
 
-Deno.test("test subtract", () => {
+Deno.test("subtraction", () => {
     // distributive: x * (y - z) = (x * y) - (x * z)
-    assertEquals(2 * op.sub(11, 7), op.sub(2 * 11, 2 * 7));
+    assertStrictEquals(
+        2 * op.sub(11, 7),
+        op.sub(2 * 11, 2 * 7),
+        "distributive",
+    );
     // identity: x - 0 = x
-    assertEquals(op.sub(7, 0), 7);
+    assertStrictEquals(op.sub(7, 0), 7, "identity");
 });
 
-Deno.test("test multiply", () => {
+Deno.test("multiplication", () => {
     // commutative: x * y = y * x
-    assertEquals(op.mul(2, 7), op.mul(7, 2));
+    assertStrictEquals(op.mul(2, 7), op.mul(7, 2), "commutative");
     // associative: (x * y) * z = x * (y * z)
-    assertEquals(op.mul(op.mul(3, 4), 5), op.mul(3, op.mul(4, 5)));
+    assertStrictEquals(
+        op.mul(op.mul(3, 4), 5),
+        op.mul(3, op.mul(4, 5)),
+        "associative",
+    );
     // distributive: x * (y + z) = (x * y) + (x * z)
-    assertEquals(op.mul(2, 7 + 11), op.mul(2, 7) + op.mul(2, 11));
+    assertStrictEquals(
+        op.mul(2, 7 + 11),
+        op.mul(2, 7) + op.mul(2, 11),
+        "distributive",
+    );
     // identity: x * 1 = x
-    assertEquals(op.mul(7, 1), 7);
+    assertStrictEquals(op.mul(7, 1), 7, "identity");
 });
 
-Deno.test("test divide", () => {
+Deno.test("division", () => {
     // identity: x / 1 = x
-    assertEquals(op.div(7, 1), 7);
+    assertStrictEquals(op.div(7, 1), 7, "identity");
     // divide by self: x / x = 1
-    assertEquals(op.div(7, 7), 1);
+    assertStrictEquals(op.div(7, 7), 1, "divide by self");
 });
 
-Deno.test("test exponent", () => {
+Deno.test("exponent", () => {
     // right associative
-    assertEquals(op.exp(2, op.exp(3, 4)), 2 ** 3 ** 4);
+    assertStrictEquals(
+        op.exp(2, op.exp(3, 4)),
+        2 ** 3 ** 4,
+        "right associative",
+    );
 });
 
-Deno.test("test remainder", () => {
+Deno.test("remainder", () => {
     // positive dividend
-    assertEquals(op.rem(11, -5), 1);
+    assertStrictEquals(op.rem(11, -5), 1, "positive dividend");
     // negative dividend
-    assertEquals(op.rem(-11, 5), -1);
+    assertStrictEquals(op.rem(-11, 5), -1, "negative dividend");
 });
 
-Deno.test("test sum", () => {
+Deno.test("sum", () => {
     // associative
-    assertEquals(op.sum(1, 2, 3), op.sum(3, 2, 1));
+    assertStrictEquals(op.sum(1, 2, 3), op.sum(3, 2, 1), "associative");
     // identity
-    assertEquals(op.sum(7), 7);
+    assertStrictEquals(op.sum(7), 7, "identity");
 });
 
-Deno.test("test product", () => {
+Deno.test("product", () => {
     // associative
-    assertEquals(op.product(2, 3, 4), op.product(4, 3, 2));
+    assertStrictEquals(op.product(2, 3, 4), op.product(4, 3, 2), "associative");
     // identity
-    assertEquals(op.product(7), 7);
+    assertStrictEquals(op.product(7), 7, "identity");
 });
