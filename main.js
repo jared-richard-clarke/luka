3dc9040bfb76e4d82fcff0bf5a75cf5e4929eba4
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-03-21 11:56:37 -0700

    Simplify documentation

--- main.js
+++ main.js
@@ -1,33 +1,24 @@
-// Provides LISP-like functional replacements
+// Module provides LISP-like functional replacements
 // for a handful of infix arithmetic operations.
-//
-// module: readonly {
-//    neg,
-//    add,
-//    sub,
-//    mul,
-//    div,
-//    exp,
-//    rem,
-//    sum,
-//    product
-// }
 
+// Factory function produces unary functions.
 function unary(operation) {
     return Object.freeze(function (x) {
         return operation(x);
     });
 }
 
+// Factory function produces binary functions.
 function binary(operation) {
     return Object.freeze(function (x, y) {
         return operation(x, y);
     });
 }
 
+// Factory function produces folding functions
+// with variable arity — in this case, monoids.
 // A monoid is a set equipped with an associative
 // binary operation and an identity element.
-// A monoid is an aggregation pattern.
 function monoid(operation, identity) {
     return Object.freeze(function (...operands) {
         return operands.reduce(
@@ -37,52 +28,22 @@ function monoid(operation, identity) {
     });
 }
 
+// "op" acts as namespace for arithmetic functions.
+// Null prototype prevents namespace pollution.
 const op = Object.create(null);
 
-// neg(number) -> -number
-// Flips the sign of a number.
-// neg(7) -> -7
+// Unary Operations
 op.neg = unary((x) => -x);
 
-// add(number, number) -> number
-// Returns the sum of two numbers.
-// add(3, 4) -> 7
-op.add = binary((x, y) => x + y);
+// Binary Operations
+op.add = binary((x, y) => x + y); // <-------- addition
+op.sub = binary((x, y) => x - y); // <-------- subtraction
+op.mul = binary((x, y) => x * y); // <-------- multiplication
+op.div = binary((x, y) => x / y); // <-------- division
+op.exp = binary((x, y) => x ** y); // <------- exponent
+op.rem = binary((x, y) => x % y); // <-------- remainder
+op.sum = monoid((x, y) => x + y, 0); // <----- sum
+op.product = monoid((x, y) => x * y, 1); // <- product
 
-// sub(number, number) -> number
-// Returns the difference of two numbers.
-// sub(11, 4) -> 7
-op.sub = binary((x, y) => x - y);
-
-// mul(number, number) -> number
-// Returns the product of two numbers.
-// mul(2, 7) -> 14
-op.mul = binary((x, y) => x * y);
-
-// div(number, number) -> number
-// Returns the quotient of two numbers.
-// div(10, 2) -> 5
-op.div = binary((x, y) => x / y);
-
-// exp(number, number) -> number
-// Returns the first number to the power of the other.
-// exp(2, 8) -> 256
-op.exp = binary((x, y) => x ** y);
-
-// rem(number, number) -> number
-// Returns the remainder of the first number divided by the other.
-// The remainder always has the same sign as the dividend.
-// rem(11, 2) -> 1
-op.rem = binary((x, y) => x % y);
-
-// sum(...number) -> number
-// Returns the sum of a variable arity of numbers.
-// sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10) -> 55
-op.sum = monoid((x, y) => x + y, 0);
-
-// product(...number) -> number
-// Returns the product of a variable arity of numbers.
-// product(2, 2, 2) -> 8
-op.product = monoid((x, y) => x * y, 1);
-
+// Make "op" immutable prior to export.
 export default Object.freeze(op);
