2a7b5687b3c742b81287cc2d94eeb1df1819ae05
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-03-30 13:36:00 -0700

    Create deps.ts

---
+++ deps.ts
@@ -0,0 +1,6 @@
+// Module consolidates dependencies in a single file
+// for easy modification.
+export {
+    assert,
+    assertStrictEquals,
+} from "https://deno.land/std@0.180.0/testing/asserts.ts";
