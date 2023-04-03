8084a14d1f3a0531bd425a84ba2ea4b4503c8593
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-04-03 14:00:23 -0700

    Replace arrow functions

--- luka.js
+++ luka.js
@@ -19,7 +19,9 @@ function binary(operation) {
 function monoid(operation, identity) {
     return Object.freeze(function (...operands) {
         return operands.reduce(
-            (total, operand) => operation(total, operand),
+            function (total, operand) {
+                return operation(total, operand);
+            },
             identity,
         );
     });
@@ -31,28 +33,60 @@ function monoid(operation, identity) {
 const op = Object.create(null);
 
 // Unary Operations
-op.neg = unary((x) => -x);
-op.not = unary((x) => !x);
+op.neg = unary(function (x) {
+    return -x;
+});
+op.not = unary(function (x) {
+    return !x;
+});
 
 // Binary Operations
-op.add = binary((x, y) => x + y);
-op.sub = binary((x, y) => x - y);
-op.mul = binary((x, y) => x * y);
-op.div = binary((x, y) => x / y);
-op.pow = binary((x, y) => Math.pow(x, y));
-op.rem = binary((x, y) => x % y);
+op.add = binary(function (x, y) {
+    return x + y;
+});
+op.sub = binary(function (x, y) {
+    return x - y;
+});
+op.mul = binary(function (x, y) {
+    return x * y;
+});
+op.div = binary(function (x, y) {
+    return x / y;
+});
+op.pow = binary(function (x, y) {
+    return Math.pow(x, y);
+});
+op.rem = binary(function (x, y) {
+    return x % y;
+});
 
 // Binary Boolean Operations
-op.eq = binary((x, y) => x === y);
-op.ne = binary((x, y) => x !== y);
-op.lt = binary((x, y) => x < y);
-op.le = binary((x, y) => x <= y);
-op.gt = binary((x, y) => x > y);
-op.ge = binary((x, y) => x >= y);
+op.eq = binary(function (x, y) {
+    return x === y;
+});
+op.ne = binary(function (x, y) {
+    return x !== y;
+});
+op.lt = binary(function (x, y) {
+    return x < y;
+});
+op.le = binary(function (x, y) {
+    return x <= y;
+});
+op.gt = binary(function (x, y) {
+    return x > y;
+});
+op.ge = binary(function (x, y) {
+    return x >= y;
+});
 
 // Folding Operations
-op.sum = monoid((x, y) => x + y, 0);
-op.product = monoid((x, y) => x * y, 1);
+op.sum = monoid(function (x, y) {
+    return x + y;
+}, 0);
+op.product = monoid(function (x, y) {
+    return x * y;
+}, 1);
 
 /**
  * Module `luka.js` provides functional replacements
@@ -60,33 +94,33 @@ op.product = monoid((x, y) => x * y, 1);
  *
  * @example
  * ```js
-* import op from "./luka.js";
-*
-* // Unary Operations
-* const negation         = op.neg(7) // ----------->    -7
-* const boolean_negation = op.not(7 === 11) // ---->  true
-*
-* // Binary Operations
-* const addition         = op.add(1, 6) // -------->     7
-* const subtraction      = op.sub(8, 1) // -------->     7
-* const multiplication   = op.mul(2, 7) // -------->    14
-* const division         = op.div(14, 2) // ------->     7
-* const power            = op.pow(2, 7) // -------->   128
-* const remainder        = op.rem(15, 7) // ------->     1
-*
-* // Binary Boolean Operations
-* const equal            = op.eq(7, 7) // --------->  true
-* const not_equal        = op.ne(7, 7) // ---------> false
-* const less             = op.lt(7, 11) // -------->  true
-* const less_equal       = op.le(11, 11) // ------->  true
-* const greater          = op.gt(7, 11) // --------> false
-* const greater_equal    = op.ge(11, 11) // ------->  true
-*
-* // Folding Operations
-* const sum              = op.sum(1, 2, 3) // ----->     6
-* const sum_id           = op.sum() // ------------>     0
-* const product          = op.product(2, 2, 2) // ->     8
-* const product_id       = op.product() // -------->     1
-*```
-*/
+ * import op from "./luka.js";
+ *
+ * // Unary Operations
+ * const negation         = op.neg(7) // ----------->    -7
+ * const boolean_negation = op.not(7 === 11) // ---->  true
+ *
+ * // Binary Operations
+ * const addition         = op.add(1, 6) // -------->     7
+ * const subtraction      = op.sub(8, 1) // -------->     7
+ * const multiplication   = op.mul(2, 7) // -------->    14
+ * const division         = op.div(14, 2) // ------->     7
+ * const power            = op.pow(2, 7) // -------->   128
+ * const remainder        = op.rem(15, 7) // ------->     1
+ *
+ * // Binary Boolean Operations
+ * const equal            = op.eq(7, 7) // --------->  true
+ * const not_equal        = op.ne(7, 7) // ---------> false
+ * const less             = op.lt(7, 11) // -------->  true
+ * const less_equal       = op.le(11, 11) // ------->  true
+ * const greater          = op.gt(7, 11) // --------> false
+ * const greater_equal    = op.ge(11, 11) // ------->  true
+ *
+ * // Folding Operations
+ * const sum              = op.sum(1, 2, 3) // ----->     6
+ * const sum_id           = op.sum() // ------------>     0
+ * const product          = op.product(2, 2, 2) // ->     8
+ * const product_id       = op.product() // -------->     1
+ * ```
+ */
 export default Object.freeze(op);
