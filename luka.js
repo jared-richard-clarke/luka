042585e51d92b20f22ddb5e74fb0186115040cb0
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-04-21 01:00:18 -0700

    Revise doc comments

--- luka.js
+++ luka.js
@@ -38,7 +38,8 @@ op.product = monoid((x, y) => x * y, 1);
 
 /**
  * Module `luka.js` provides functional replacements
- * for a handful of infix arithmetic operations.
+ * for a handful of infix arithmetic operations:
+ * [ `+`, `-`, `*`, `/`, `**`, `%`, `===`].
  *
  * @example
  * ```js
