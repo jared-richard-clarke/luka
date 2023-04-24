b41da5e0bda5587dd228bcdee07d3e661dab84f7
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-04-24 10:27:10 -0700

    Add comparison operations

--- luka.js
+++ luka.js
@@ -30,7 +30,14 @@ op.mul = binary((x, y) => x * y);
 op.div = binary((x, y) => x / y);
 op.exp = binary((x, y) => Math.pow(x, y));
 op.rem = binary((x, y) => x % y);
-op.equal = binary((x, y) => x === y);
+
+// Binary Comparison Operations
+op.eq = binary((x, y) => x === y);
+op.ne = binary((x, y) => x !== y);
+op.lt = binary((x, y) => x < y);
+op.le = binary((x, y) => x <= y);
+op.gt = binary((x, y) => x > y);
+op.ge = binary((x, y) => x >= y);
 
 // Folding Operations
 op.sum = monoid((x, y) => x + y, 0);
@@ -38,24 +45,31 @@ op.product = monoid((x, y) => x * y, 1);
 
 /**
  * Module `luka.js` provides functional replacements
- * for a handful of infix arithmetic operations:
+ * for a handful of infix operations:
  *
- * [ `+`, `-`, `*`, `/`, `**`, `%`, `===`].
+ * [ `+`, `-`, `*`, `/`, `**`, `%`, `===`, `!==`, `<`, `<=`, `>`, `>=`].
  *
  * @example
  * ```js
  * import op from "./luka.js";
  *
- * // Binary Operations
+ * // Binary Arithmetic Operations
  * const addition         = op.add(1, 6) // -------->     7
  * const subtraction      = op.sub(8, 1) // -------->     7
  * const multiplication   = op.mul(2, 7) // -------->    14
  * const division         = op.div(14, 2) // ------->     7
  * const exponent         = op.exp(2, 7) // -------->   128
  * const remainder        = op.rem(15, 7) // ------->     1
- * const equality         = op.equal(7, 7) // ------>  true
  *
- * // Folding Operations
+ * // Binary Comparison Operations
+ * const equal            = op.eq(7, 7) // --------->  true
+ * const not_equal        = op.ne(7, 7) // ---------> false
+ * const lesser           = op.lt(7, 11) // -------->  true
+ * const lesser_equal     = op.le(7, 11) // -------->  true
+ * const greater          = op.gt(7, 11) // --------> false
+ * const greater_equal    = op.ge(7, 11) // --------> false
+ *
+ * // Folding Arithmetic Operations
  * const sum              = op.sum(1, 2, 3) // ----->     6
  * const sum_id           = op.sum() // ------------>     0
  * const product          = op.product(2, 2, 2) // ->     8
