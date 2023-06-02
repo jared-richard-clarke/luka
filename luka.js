9af7b07d446b894e535510b490ddfaadec9bb7c2
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-06-01 17:01:18 -0700

    Shrink API

--- luka.js
+++ luka.js
@@ -5,10 +5,8 @@ function binary(operation) {
     });
 }
 
-// Factory function that produces folding functions
-// with variable arity — in this case semigroups.
-// A semigroup is a set with an associative
-// binary operation.
+// Factory function that produces folding functions with variable arity — in this case semigroups.
+// A semigroup is a set with an associative binary operation.
 function semigroup(operation) {
     return Object.freeze(function (...xs) {
         return xs.reduce(
@@ -17,10 +15,8 @@ function semigroup(operation) {
     });
 }
 
-// Factory function that produces folding functions
-// with variable arity — in this case, monoids.
-// A monoid is a set equipped with an associative
-// binary operation AND an identity element.
+// Factory function that produces folding functions with variable arity — in this case, monoids.
+// A monoid is a set equipped with an associative binary operation AND an identity element.
 function monoid(operation, identity) {
     return Object.freeze(function (...operands) {
         return operands.reduce(
@@ -30,8 +26,7 @@ function monoid(operation, identity) {
     });
 }
 
-// "op" acts as namespace for arithmetic functions.
-// Its null prototype prevents namespace pollution
+// "op" acts as namespace for arithmetic functions. Its null prototype prevents namespace pollution
 // from inherited objects.
 const op = Object.create(null);
 
@@ -43,45 +38,32 @@ op.div = semigroup((x, y) => x / y);
 op.exp = binary((x, y) => Math.pow(x, y));
 op.rem = binary((x, y) => x % y);
 
-// Binary Comparison Operations
-op.eq = binary((x, y) => x === y);
-op.ne = binary((x, y) => x !== y);
-op.lt = binary((x, y) => x < y);
-op.le = binary((x, y) => x <= y);
-op.gt = binary((x, y) => x > y);
-op.ge = binary((x, y) => x >= y);
-
 /**
- * Module `luka.js` provides functional replacements
- * for a handful of infix operations:
+ * Module `luka.js` provides functional replacements for a handful of infix, arithmetic operations:
  *
- * [ `+`, `-`, `*`, `/`, `**`, `%`, `===`, `!==`, `<`, `<=`, `>`, `>=`].
+ * [ `+`, `-`, `*`, `/`, `**`, `%`].
  *
  * @example
  * ```js
  * import op from "./luka.js";
  *
  * // Binary Arithmetic Operations
- * const addition         = op.add(1, 6) // -------->     7
- * const subtraction      = op.sub(8, 1) // -------->     7
- * const multiplication   = op.mul(2, 7) // -------->    14
- * const division         = op.div(14, 2) // ------->     7
- * const exponent         = op.exp(2, 7) // -------->   128
- * const remainder        = op.rem(15, 7) // ------->     1
+ * const addition         = op.add(1, 6); // --------->   7
+ * const subtraction      = op.sub(8, 1); // --------->   7
+ * const multiplication   = op.mul(2, 7); // --------->  14
+ * const division         = op.div(14, 2); // -------->   7
+ * const exponent         = op.exp(2, 7); // ---------> 128
+ * const remainder        = op.rem(15, 7); // -------->   1
  *
- * // Binary Comparison Operations
- * const equal            = op.eq(7, 7) // --------->  true
- * const not_equal        = op.ne(7, 7) // ---------> false
- * const lesser           = op.lt(7, 11) // -------->  true
- * const lesser_equal     = op.le(7, 11) // -------->  true
- * const greater          = op.gt(7, 11) // --------> false
- * const greater_equal    = op.ge(7, 11) // --------> false
- * 
  * // Monoid Operations
- * const sum              = op.add(1, 2, 3) // ----->     6
- * const sum_id           = op.add() // ------------>     0
- * const product          = op.mul(2, 2, 2) // ----->     8
- * const product_id       = op.mul() // ------------>     1
+ * const sum              = op.add(1, 2, 3); // ------>   6
+ * const sum_id           = op.add(); // ------------->   0
+ * const product          = op.mul(2, 4, 8, 10); // --> 640
+ * const product_id       = op.mul(); // ------------->   1
+ *
+ * // Semigroup Operations
+ * const difference       = op.sub(10, 5, 5); // ----->   0
+ * const quotient         = op.div(1000, 10, 10); // ->  10
  * ```
  */
 export default Object.freeze(op);
